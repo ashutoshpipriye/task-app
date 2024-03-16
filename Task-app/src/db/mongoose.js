70456e166ae8e634cd497114f1ddaaa0fcb8d08e
ashutoshpipriye <ashutoshpipriye123@gmail.com>
2024-03-17 01:39:44 +0530

    Dockerize the node and mongo db application

--- Task-app/src/db/mongoose.js
+++ Task-app/src/db/mongoose.js
@@ -1,3 +1,33 @@
-const mongoose = require("mongoose");
+const dotenv = require('dotenv').config();
+const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api");
+mongoose
+    .connect(process.env.MONGODB_URI, {
+        dbName: process.env.DB_NAME,
+        useNewUrlParser: true,
+    })
+    .then(() => {
+        console.log('Mongodb connected....');
+    })
+    .catch(err => console.log(err.message));
+
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose connected to db...');
+});
+
+mongoose.connection.on('error', err => {
+    console.log(err.message);
+});
+
+mongoose.connection.on('disconnected', () => {
+    console.log('Mongoose connection is disconnected...');
+});
+
+process.on('SIGINT', () => {
+    mongoose.connection.close(() => {
+        console.log(
+            'Mongoose connection is disconnected due to app termination...'
+        );
+        process.exit(0);
+    });
+});
